f4001aff1c125c58c6e2843f3bf8f59d4c9b7849
Sonja <sonjaunger@eyewant.me>
2022-08-29 04:00:27 -0700

    ShittyShadow
    Politur

---
+++ 5_ShittyShadow/exercise/src/script.js
@@ -0,0 +1,168 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+scene.add(ambientLight)
+
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+directionalLight.position.set(2, 2, - 1)
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
+scene.add(directionalLight)
+
+directionalLight.castShadow = true
+
+/*
+directionalLight.shadow.mapSize.with = 1024
+directionalLight.shadow.mapSize.height = 1024
+*/
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.7
+gui.add(material, 'metalness').min(0).max(1).step(0.001)
+gui.add(material, 'roughness').min(0).max(1).step(0.001)
+
+/**
+ * Objects
+ */
+// Sphere
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
+sphere.position.x = - 1.5
+sphere.castShadow = true
+
+// Cube
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+)
+cube.position.x = 0.5
+
+// Capsule
+const capsule = new THREE.Mesh(
+    new THREE.CapsuleGeometry(0.25, 0.5, 64,128), // *4
+    material
+)
+capsule.position.x = 2.5
+
+// Donut
+const donut = new THREE.Mesh(
+    new THREE.TorusGeometry(0.6, 0.075, 64, 128), // ,,16,32
+    material
+)
+donut.position.x = 2.5
+donut.rotation.x = 1
+
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(8, 4),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+plane.position.z = 0
+plane.receiveShadow = true
+
+scene.add(sphere, cube, capsule, donut, plane)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.shadowMap.enabled = true
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Update objects
+    sphere.rotation.x = 0.15 * elapsedTime
+    sphere.rotation.y = 0.1 * elapsedTime
+    cube.rotation.x = 0.15 * elapsedTime
+    cube.rotation.y = 0.1 * elapsedTime
+    donut.rotation.x = elapsedTime * 2
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
