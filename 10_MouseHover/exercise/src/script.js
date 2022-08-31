172918e8a0aa412c572758eba8fc5f4452bacee2
Sonja <sonjaunger@eyewant.me>
2022-08-30 19:48:37 -0700

    MouseHover
    ready

--- 10_MouseHover/exercise/src/script.js
+++ 10_MouseHover/exercise/src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import {Ray} from "three";
+import {int} from "three/examples/jsm/nodes/shadernode/ShaderNodeBaseElements";
 
 /**
  * Base
@@ -17,42 +18,132 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg')
+
+/**
+ * Lights
+ */
+// Directional Light
+const directionalLight = new THREE.DirectionalLight(0xF7D5E0, 0.5)
+directionalLight.position.set(4, 2, - 1)
+scene.add(directionalLight)
+
+// Spot Light
+const spotLight = new THREE.SpotLight(0xED3080, 0.5, 10, Math.PI * 0.3)
+spotLight.position.set(0,2,4)
+scene.add(spotLight)
+scene.add(spotLight.target)
+
+// Point Light
+const pointLight = new THREE.PointLight(0xF7D5E0, 0.5)
+pointLight.position.set(0, 3, 0)
+scene.add(pointLight)
+
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.7
+material.color = new THREE.Color(0xF7D5E0) // F7D5E0 // 4e00ff 
+
+const groundMaterial = new THREE.MeshStandardMaterial()
+material.color = new THREE.Color(0xF7D5E0)
+gui.add(material, 'metalness').min(0).max(1).step(0.001)
+gui.add(material, 'roughness').min(0).max(1).step(0.001)
+
+/**
  * Objects
  */
-const object1 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: 'gold' })
+// Ground
+const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(8, 4),
+    groundMaterial
 )
-object1.position.x = - 2
+ground.rotation.x = - Math.PI * 0.5
+ground.position.y = -1.5
+ground.position.z = 0
 
-const object2 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: 'gold' })
+// Sphere1
+const sphere1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
 )
+sphere1.position.x = - 2
+sphere1.castShadow = false
 
-const object3 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: 'gold' })
+// Sphere 2
+const sphere2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
 )
-object3.position.x = 2
+sphere2.position.x = 0
+sphere2.castShadow = false
 
-scene.add(object1, object2, object3)
+scene.add(ground, sphere1, sphere2)
+
+// Sphere 3
+const sphere3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
+sphere3.position.x = 2
+sphere3.castShadow = false
+
+// Add objects to scene
+scene.add(sphere1, sphere2, sphere3, ground)
+
+/**
+* Shadows
+*/
+// Shadow Sphere 1
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000, // ED3080
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShadow.rotation.x = - Math.PI * 0.5
+sphereShadow.position.y = ground.position.y + 0.01
+scene.add(sphereShadow, sphere1,  ground)
+
+// Shadow Sphere 2
+const sphereShadow2 = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000, // ED3080
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShadow2.rotation.x = - Math.PI * 0.5
+sphereShadow2.position.y = ground.position.y + 0.011
+sphereShadow2.position.x = ground.position.x - 2
+scene.add(sphereShadow2, sphere2,  ground)
+
+// Shadow Sphere 3
+const sphereShadow3 = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000, // ED3080
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShadow3.rotation.x = - Math.PI * 0.5
+sphereShadow3.position.x = ground.position.x + 2
+sphereShadow3.position.y = ground.position.y + 0.011
+scene.add(sphereShadow3, sphere3,  ground)
 
 /**
  * Raycaster
  */
 const raycaster = new THREE.Raycaster()
-
-const rayOrigin = new THREE.Vector3(-3, 0, 0)
-const rayDirection = new THREE.Vector3(10,0,0)
-rayDirection.normalize()
-raycaster.set(rayDirection, rayDirection)
-
-const intersect = raycaster.intersectObject(object2)
-console.log(intersect)
-
-const intersects = raycaster.intersectObjects([object1, object2, object3])
-console.log(intersects)
 
 /**
  * Sizes
@@ -85,32 +176,8 @@ const mouse = new THREE.Vector2()
 window.addEventListener('mousemove', (event) =>
 {
     mouse.x = event.clientX / sizes.width * 2 - 1
-    mouse.y = -(event.clientY / sizes.height * 2 - 1)
-})
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1
 
-window.addEventListener('click', () =>
-{
-    if(currentIntersect)
-    {
-        if(currentIntersect.object === object1)
-        {
-            console.log('1click')
-        }
-    }
-    if(currentIntersect)
-    {
-        if(currentIntersect.object === object2)
-        {
-            console.log('2click')
-        }
-    }
-    if(currentIntersect)
-    {
-        if(currentIntersect.object === object3)
-        {
-            console.log('3click')
-        }
-    }
 })
 
 /**
@@ -118,7 +185,9 @@ window.addEventListener('click', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 8
 scene.add(camera)
 
 // Controls
@@ -139,58 +208,43 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-let currentIntersect = null
-
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
     
     // Animate objects
-    object1.position.y = Math.sin(elapsedTime * 1.4) * 1.5
-    object2.position.y = Math.sin(elapsedTime * 1.6) * 1.5
-    object3.position.y = Math.sin(elapsedTime * 1.8) * 1.5
+    sphere1.position.y = Math.sin(elapsedTime * 2.4)
+    sphere2.position.y = Math.sin(elapsedTime * 2.6)
+    sphere3.position.y = Math.sin(elapsedTime * 2.8)
 
+    // Animate the shadow
+    sphereShadow.position.x = sphere1.position.x
+    sphereShadow.position.z = sphere1.position.z
+    sphereShadow.material.opacity = (1 - sphere1.position.y) * 0.42
+
+    sphereShadow2.position.x = sphere2.position.x
+    sphereShadow2.position.z = sphere2.position.z
+    sphereShadow2.material.opacity = (1 - sphere2.position.y) * 0.42
+
+    sphereShadow3.position.x = sphere3.position.x
+    sphereShadow3.position.z = sphere3.position.z
+    sphereShadow3.material.opacity = (1 - sphere3.position.y) * 0.42
+    
+    
     // Cast a ray
     raycaster.setFromCamera(mouse, camera)
-    
-    
-    /*
-    const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-    const rayDirection = new THREE.Vector3(1, 0, 0)
-    rayDirection.normalize()
 
-    raycaster.set(rayOrigin, rayDirection)
-    */
-    
-    const objectsToTest = [object1, object2, object3]
+    const objectsToTest = [sphere1, sphere2, sphere3]
     const intersects = raycaster.intersectObjects(objectsToTest)
-    
+
     for(const object of objectsToTest)
     {
-        object.material.color.set('gold')
-    }
-
-    for(const intersect of intersects)
-    {
-        intersect.object.material.color.set('#0000ff')
+        object.material.color.set('#F7D5E0')
     }
     
-    if(intersects.length)
+    for(const intersect of intersects)
     {
-        if(currentIntersect === null)
-        {
-            console.log('mouse enter')
-        }
-        currentIntersect = intersects[0]
-        currentIntersect = intersects[0]
-    }
-    else
-    {
-        if(currentIntersect === null)
-        {
-            console.log('out')
-        }
-        currentIntersect = null
+        intersect.object.material.color.set('#ED3080')
     }
     
     // Update controls
@@ -202,5 +256,4 @@ const tick = () =>
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
-
 tick()
